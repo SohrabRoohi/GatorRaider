98be6bff4299f5dbd472a33e50e24eac586594e3
Sohrab Roohi <sohrabroohi@ufl.edu>
2017-12-02 17:22:05 -0500

    Method change

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -9,66 +9,67 @@ import java.lang.reflect.Array;
 import java.util.List;
 import java.util.ArrayList;
 
-public final class StudentController implements DefenderController
-{
-	public void init(Game game) { }
+public final class StudentController implements DefenderController {
+	public void init(Game game) {
+	}
 
-	public void shutdown(Game game) { }
+	public void shutdown(Game game) {
+	}
 
-	public int[] update(Game game,long timeDue)
-	{
+	public int[] update(Game game, long timeDue) {
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
-		
+
 		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
 		//any random number of all of the ghosts
-		for(int i = 0; i < actions.length; i++)
-		{
-			List<Node> attackerPossibleLocations = game.getAttacker().getPossibleLocations(true);
-			int attackerX = -1;
-			int attackerY = -1;
-			for(int j = 0; j < 3; j++) {
-				if(attackerPossibleLocations.get(j) != null) {
-					attackerX = attackerPossibleLocations.get(j).getX();
-					attackerY = attackerPossibleLocations.get(j).getY();
-				}
-			}
-			Defender defender = enemies.get(i);
-			List<Node> defenderPossibleLocations = defender.getPossibleLocations();
-			int defenderX = -1;
-			int defenderY = -1;
-			for(int j = 0; j < 3; j++) {
-				if(defenderPossibleLocations.get(j) != null) {
-					defenderX = defenderPossibleLocations.get(j).getX();
-					defenderY = defenderPossibleLocations.get(j).getY();
-				}
-			}
-			int xDistance = Math.abs(attackerX - defenderX);
-			int yDistance = Math.abs(attackerY - defenderY);
-			List<Integer> possibleDirs = defender.getPossibleDirs();
-			if (possibleDirs.size() != 0)
-				// 0 is up
-				// 1 is right
-				// 2 is down
-				// 3 is left
-				if(defenderX > attackerX && possibleDirs.contains(3) && xDistance > yDistance) {
-					actions[i] = 3;
-				}
-				else if(defenderX < attackerX && possibleDirs.contains(1) && xDistance > yDistance) {
-					actions[i] = 1;
-				}
-				else if(defenderY > attackerY && possibleDirs.contains(0) && yDistance > xDistance) {
-					actions[i] = 0;
-				}
-				else if(defenderY < attackerY && possibleDirs.contains(2) && yDistance > xDistance) {
-					actions[i] = 2;
-				}
-				else {
-					actions[i] = -1;
-				}
-			else
-				actions[i] = -1;
-		}
+		actions[0] = firstDefenderAction(enemies.get(0), game);
+		actions[1] = firstDefenderAction(enemies.get(1), game);
+		actions[2] = firstDefenderAction(enemies.get(2), game);
+		actions[3] = firstDefenderAction(enemies.get(3), game);
 		return actions;
 	}
+
+	public int firstDefenderAction(Defender defender, Game game) {
+		List<Node> attackerPossibleLocations = game.getAttacker().getPossibleLocations(true);
+		int attackerX = -1;
+		int attackerY = -1;
+		for (int j = 0; j < 3; j++) {
+			if (attackerPossibleLocations.get(j) != null) {
+				attackerX = attackerPossibleLocations.get(j).getX();
+				attackerY = attackerPossibleLocations.get(j).getY();
+			}
+		}
+		List<Node> defenderPossibleLocations = defender.getPossibleLocations();
+		int defenderX = -1;
+		int defenderY = -1;
+		for (int j = 0; j < 3; j++) {
+			if (defenderPossibleLocations.get(j) != null) {
+				defenderX = defenderPossibleLocations.get(j).getX();
+				defenderY = defenderPossibleLocations.get(j).getY();
+			}
+		}
+		int xDistance = Math.abs(attackerX - defenderX);
+		int yDistance = Math.abs(attackerY - defenderY);
+		List<Integer> possibleDirs = defender.getPossibleDirs();
+		if (possibleDirs.size() != 0)
+			// 0 is up
+			// 1 is right
+			// 2 is down
+			// 3 is left
+			if (defenderX > attackerX && possibleDirs.contains(3) && xDistance > yDistance) {
+				return 3;
+			} else if (defenderX < attackerX && possibleDirs.contains(1) && xDistance > yDistance) {
+				return 1;
+			} else if (defenderY > attackerY && possibleDirs.contains(0) && yDistance > xDistance) {
+				return 0;
+			} else if (defenderY < attackerY && possibleDirs.contains(2) && yDistance > xDistance) {
+				return 2;
+			} else {
+				return -1;
+			}
+		else
+			return -1;
+	}
+
 }
+
